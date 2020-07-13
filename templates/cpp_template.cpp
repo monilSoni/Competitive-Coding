0a84d9b389efb17f8afbcc1aa2e53a9337a60474
Monil Soni <monisoni@amazon.com>
2020-07-13 11:44:59 +0530

    Added stdc++.h file for using it in MacOS and added CPP template.

---
+++ templates/cpp_template.cpp
@@ -0,0 +1,24 @@
+/* Monil Soni sonil06 */
+ 
+#include "/Users/monisoni/Competitive-Coding/stdc++.h"
+#include <unistd.h>
+ 
+using namespace std;
+ 
+typedef long long LL;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int, int> pi;
+ 
+#define MOD 1000000007
+#define rep(i,s,n) for(int i=s; i<n; i++)
+#define repp(i,s,n) for(int i=s; i<=n; i++)
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define CLOCK_INIT clock_t start, stop; double t = 0.0; assert((start = clock())!=-1);
+#define CLOCK_END stop = clock(); t = (double) (stop-start)/CLOCKS_PER_SEC; printf("Run time: %f\n", t); 
+
